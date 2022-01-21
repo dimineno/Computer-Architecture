faf7213c5e86ccccae5c5e367d09838ee5ca95fb
dimineno <95146340+dimineno@users.noreply.github.com>
2022-01-21 13:14:26 +0200

    Add files via upload

---
+++ Lab_3/generate.sh
@@ -0,0 +1,257 @@
+#!/bin/bash
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_0/stats.txt Lab_2/spec_results/specbzip_0/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_0.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_1/stats.txt Lab_2/spec_results/specbzip_1/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_1.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_2/stats.txt Lab_2/spec_results/specbzip_2/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_2.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_3/stats.txt Lab_2/spec_results/specbzip_3/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_3.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_4/stats.txt Lab_2/spec_results/specbzip_4/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_4.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_5/stats.txt Lab_2/spec_results/specbzip_5/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_5.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_6/stats.txt Lab_2/spec_results/specbzip_6/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_6.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_7/stats.txt Lab_2/spec_results/specbzip_7/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_7.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_8/stats.txt Lab_2/spec_results/specbzip_8/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_8.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_9/stats.txt Lab_2/spec_results/specbzip_9/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_9.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_10/stats.txt Lab_2/spec_results/specbzip_10/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_10.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_11/stats.txt Lab_2/spec_results/specbzip_11/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_11.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_12/stats.txt Lab_2/spec_results/specbzip_12/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_12.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_13/stats.txt Lab_2/spec_results/specbzip_13/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_13.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_14/stats.txt Lab_2/spec_results/specbzip_14/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_14.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_15/stats.txt Lab_2/spec_results/specbzip_15/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_15.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_16/stats.txt Lab_2/spec_results/specbzip_16/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_16.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_17/stats.txt Lab_2/spec_results/specbzip_17/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_17.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_18/stats.txt Lab_2/spec_results/specbzip_18/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_18.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specbzip_19/stats.txt Lab_2/spec_results/specbzip_19/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specbzip/specbzip_19.xml
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_0/stats.txt Lab_2/spec_results/speclibm_0/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_0.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_1/stats.txt Lab_2/spec_results/speclibm_1/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_1.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_2/stats.txt Lab_2/spec_results/speclibm_2/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_2.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_3/stats.txt Lab_2/spec_results/speclibm_3/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_3.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_4/stats.txt Lab_2/spec_results/speclibm_4/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_4.xml
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_5/stats.txt Lab_2/spec_results/speclibm_5/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_5.xml
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_6/stats.txt Lab_2/spec_results/speclibm_6/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_6.xml
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_7/stats.txt Lab_2/spec_results/speclibm_7/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_7.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_8/stats.txt Lab_2/spec_results/speclibm_8/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_8.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_9/stats.txt Lab_2/spec_results/speclibm_9/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_9.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_10/stats.txt Lab_2/spec_results/speclibm_10/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_10.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_11/stats.txt Lab_2/spec_results/speclibm_11/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_11.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_12/stats.txt Lab_2/spec_results/speclibm_12/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_12.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/speclibm_13/stats.txt Lab_2/spec_results/speclibm_13/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/speclibm/speclibm_13.xml
+
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_0/stats.txt Lab_2/spec_results/spechmmer_0/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_0.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_1/stats.txt Lab_2/spec_results/spechmmer_1/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_1.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_2/stats.txt Lab_2/spec_results/spechmmer_2/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_2.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_3/stats.txt Lab_2/spec_results/spechmmer_3/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_3.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_4/stats.txt Lab_2/spec_results/spechmmer_4/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer4.xml
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_5/stats.txt Lab_2/spec_results/spechmmer_5/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_5.xml
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_6/stats.txt Lab_2/spec_results/spechmmer_6/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_6.xml
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_7/stats.txt Lab_2/spec_results/spechmmer_7/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_7.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_8/stats.txt Lab_2/spec_results/spechmmer_8/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_8.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_9/stats.txt Lab_2/spec_results/spechmmer_9/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_9.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_10/stats.txt Lab_2/spec_results/spechmmer10/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_10.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_11/stats.txt Lab_2/spec_results/spechmmer_11/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_11.xml
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_12/stats.txt Lab_2/spec_results/spechmmer_12/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_12.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/spechmmer_13/stats.txt Lab_2/spec_results/spechmmer_13/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/spechmmer/spechmmer_13.xml
+
+
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_0/stats.txt Lab_2/spec_results/specmcf_0/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_0.xml
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_1/stats.txt Lab_2/spec_results/specmcf_1/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_1.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_2/stats.txt Lab_2/spec_results/specmcf_2/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_2.xml
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_3/stats.txt Lab_2/spec_results/specmcf_3/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_3.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_4/stats.txt Lab_2/spec_results/specmcf_4/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_4.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_5/stats.txt Lab_2/spec_results/specmcf_5/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_5.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_6/stats.txt Lab_2/spec_results/specmcf_6/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_6.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_7/stats.txt Lab_2/spec_results/specmcf_7/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_7.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_8/stats.txt Lab_2/spec_results/specmcf_8/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_8.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_9/stats.txt Lab_2/spec_results/specmcf_9/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_9.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_10/stats.txt Lab_2/spec_results/specmcf_10/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_10.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_11/stats.txt Lab_2/spec_results/specmcf_11/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_11.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_12/stats.txt Lab_2/spec_results/specmcf_12/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_12.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_13/stats.txt Lab_2/spec_results/specmcf_13/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_13.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_14/stats.txt Lab_2/spec_results/specmcf_14/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_14.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specmcf_15/stats.txt Lab_2/spec_results/specmcf_15/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specmcf/specmcf_15.xml
+
+
+
+
+
+
+
+
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_0/stats.txt Lab_2/spec_results/specsjeng_0/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_0.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_1/stats.txt Lab_2/spec_results/specsjeng_1/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_1.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_2/stats.txt Lab_2/spec_results/specsjeng_2/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_2.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_3/stats.txt Lab_2/spec_results/specsjeng_3/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_3.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_4/stats.txt Lab_2/spec_results/specsjeng_4/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_4.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_5/stats.txt Lab_2/spec_results/specsjeng_5/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_5.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_6/stats.txt Lab_2/spec_results/specsjeng_6/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_6.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_7/stats.txt Lab_2/spec_results/specsjeng_7/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_7.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_8/stats.txt Lab_2/spec_results/specsjeng_8/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_8.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_9/stats.txt Lab_2/spec_results/specsjeng_9/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_9.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_10/stats.txt Lab_2/spec_results/specsjeng_10/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_10.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_11/stats.txt Lab_2/spec_results/specsjeng_11/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_11.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_12/stats.txt Lab_2/spec_results/specsjeng_12/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_12.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_13/stats.txt Lab_2/spec_results/specsjeng_13/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_13.xml
+
+./mcpat/Scripts/GEM5ToMcPAT.py Lab_2/spec_results/specsjeng_14/stats.txt Lab_2/spec_results/specsjeng_14/config.json mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o mcpat/mcpat/ProcessorDescriptionFiles/specsjeng/specsjeng_14.xml
+
+
+
+
+
+
+
+
+
+
+
+
